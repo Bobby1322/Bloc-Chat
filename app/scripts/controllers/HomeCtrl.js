cdc1f9ab696449d9ca99f2a957fabd36f8807610
Bob-e <bob.e.mercer@gmail.com>
2017-08-28 16:24:28 -0400

    Bloc chat 5 and css 4

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,9 @@
 (function() {
-    function HomeCtrl(Room, Message, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal, $cookies) {
         var home = this;
         home.rooms = Room.all;
         home.currentRoom = null;
+        home.currentUser = $cookies.get('blocChatCurrentUser');
         
         home.addRoom = function() {
             $uibModal.open({
@@ -17,11 +18,12 @@
             home.messages = Message.getByRoomId(home.currentRoom.$id);
             console.log(home.currentRoom);
             console.log(home.messages);
+            console.log(home.currentUser);
         }
         
     }
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', '$cookies', HomeCtrl]);
 })();
